8622714c9c69f62e132dbfdd7f46dd5fccef0d57
ikoba <i.koba.jp@gmail.com>
2017-11-07 23:51:01 +0900

    Update cotroller tests

--- test/controllers/time_cards_controller_test.rb
+++ test/controllers/time_cards_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class TimeCardsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get time_cards_index_url
-  #   assert_response :success
-  # end
+  test "should get show" do
+    get timecard_url
+    assert_redirected_to login_url
+  end
 
-  # test "should get show" do
-  #   get time_cards_show_url
-  #   assert_response :success
-  # end
-
+  test "should get index" do
+    get monthly_url
+    assert_redirected_to login_url
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "should get new" do
-  #   get users_new_url
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
 
-
+  test "form class" do
+    get signup_path
+    assert_select 'form[action="/users"]'
+  end
 end
