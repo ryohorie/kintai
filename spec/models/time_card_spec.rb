449cf09af9ce164b555f22818d91a729cf5deedc
ikoba <i.koba.jp@gmail.com>
2017-11-23 23:59:50 +0900

    Update time_card_spec.rb

--- spec/models/time_card_spec.rb
+++ spec/models/time_card_spec.rb
@@ -39,7 +39,22 @@ RSpec.describe TimeCard, type: :model do
 
     context 'combination of year, month, date is an invalid date' do
       subject(:time_card) { build(:time_card, year: 2017, month: 2, day: 29) }
-      it { expect(time_card).not_to be_valid }
+      it 'is invalid' do
+        expect(time_card).not_to be_valid
+        expect(time_card.errors[:base].size).to be >= 1
+      end
+    end
+
+    context 'out_at is earlier than in_at' do
+      subject(:time_card) do
+        now = Time.zone.now
+        build(:time_card, in_at: now + 10.minutes, out_at: now)  
+      end
+
+      it 'is invalid' do
+        expect(time_card).not_to be_valid
+        expect(time_card.errors[:base].size).to be >= 1
+      end
     end
   end
 end
