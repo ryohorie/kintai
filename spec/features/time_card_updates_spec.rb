f083415381fb564f9731efc5411bfde5ced7a4ff
ikoba <i.koba.jp@gmail.com>
2017-11-29 20:25:19 +0900

    Update specs

--- spec/features/time_card_updates_spec.rb
+++ spec/features/time_card_updates_spec.rb
@@ -8,13 +8,17 @@ RSpec.feature "TimeCardUpdates", type: :feature, js: true do
     login
   end
 
-  scenario 'user comes into office' do
+  scenario 'user comes to the office and leaves the office' do
     expect(page).to have_current_path root_path    
     expect(page).to have_css '#work-status', text: '未出社'
 
     click_on '出社'
-    
+
     expect(page).to have_css '#work-status', text: '勤務中'
+
+    click_on '退社'
+
+    expect(page).to have_css '#work-status', text: '退社済'    
   end
 
   def login
